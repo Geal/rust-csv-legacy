68c8e780af4cb229828249a3133dc2defdc3c8ed
Geoffroy Couprie <geo.couprie@gmail.com>
2014-01-06 11:57:46 +0100

    fix some ownership problems

--- src/rust-csv/test.rs
+++ src/rust-csv/test.rs
@@ -1,18 +1,20 @@
 use csv = lib;
-use csv::{rowreader, new_reader, new_reader_readlen};
+use csv::{rowreader, new_reader, new_reader_readlen, rowiter};
 
 use std::str;
 use std::io;
+use io::Reader;
+use io::mem::MemReader;
 use std::vec;
 
 mod lib;
 
-    fn rowmatch(testdata: ~str, expected: ~[~[~str]]) {
-        let chk = |s: str, mk: fn(io::Reader) -> rowreader| {
-            let f = io::mem::MemReader::new(s.into_bytes());
+    fn rowmatch(testdata: &'static str, expected: ~[~[&'static str]]) {
+        let chk = |s: ~str, mk: proc(~Reader) -> ~rowreader| {
+            let f = ~MemReader::new(s.into_bytes());
             let r = mk(f);
             let mut i = 0u;
-            let mut row: [str] = [];
+            let mut row: ~[~str] = ~[];
             loop {
                 let temp = r.readrow();
                 if(temp.len() == 0) {
@@ -23,26 +25,26 @@ mod lib;
                 assert!(row.len() == expect.len());
                 let mut j = 0u;
                 while j < row.len() {
-                    assert!(row[j] == expect[j]);
+                    assert!(expect[j] == row[j]);
                     j += 1u;
                 }
                 i += 1u;
             }
             assert!(i == expected.len());
         };
-        let runchecks = |s: str| {
+        let runchecks = |s: &'static str| {
             // test default reader params
-            do chk(s) |inp| {
+            do chk(s.to_owned()) |inp| {
                 new_reader_readlen(inp, ',', '"', 2u)
             };
             // test default constructor
-            do chk(s) |inp| {
+            do chk(s.to_owned()) |inp| {
                 new_reader(inp, ',', '"')
             };
             // test continuations over read buffers
             let mut j = 1u;
             while j < s.len() {
-                do chk(s) |inp| {
+                do chk(s.to_owned()) |inp| {
                     new_reader_readlen(inp, ',', '"', j)
                 };
                 j += 1u;
@@ -62,54 +64,55 @@ mod lib;
     #[test]
     fn simple() {
         rowmatch("a,b,c,d\n1,2,3,4",
-                 [["a", "b", "c", "d"], ["1", "2", "3", "4"]]);
+                 ~[~["a", "b", "c", "d"], ~["1", "2", "3", "4"]]);
     }
 
     #[test]
     fn trailing_comma() {
         rowmatch("a,b,c,d\n1,2,3,4,",
-                 [["a", "b", "c", "d"], ["1", "2", "3", "4", ""]]);
+                 ~[~["a", "b", "c", "d"], ~["1", "2", "3", "4", ""]]);
     }
 
     #[test]
     fn leading_comma() {
         rowmatch("a,b,c,d\n,1,2,3,4",
-                 [["a", "b", "c", "d"], ["", "1", "2", "3", "4"]]);
+                 ~[~["a", "b", "c", "d"], ~["", "1", "2", "3", "4"]]);
     }
 
     #[test]
     fn quote_simple() {
-        rowmatch("\"Hello\",\"There\"\na,b,\"c\",d",
-                 [["Hello", "There"], ["a", "b", "c", "d"]]);
+        rowmatch(~"\"Hello\",\"There\"\na,b,\"c\",d",
+                 ~[~["Hello", "There"], ~["a", "b", "c", "d"]]);
     }
 
     #[test]
     fn quote_nested() {
-        rowmatch("\"Hello\",\"There is a \"\"fly\"\" in my soup\"\na,b,\"c\",d",
-                 [["Hello", "There is a \"fly\" in my soup"], ["a", "b", "c", "d"]]);
+        rowmatch(~"\"Hello\",\"There is a \"\"fly\"\" in my soup\"\na,b,\"c\",d",
+                 ~[~["Hello", "There is a \"fly\" in my soup"], ~["a", "b", "c", "d"]]);
     }
 
     #[test]
     fn quote_with_comma() {
-        rowmatch("\"1,2\"",
-                 [["1,2"]])
+        rowmatch(~"\"1,2\"",
+                 ~[~["1,2"]])
     }
 
     #[test]
     fn quote_with_other_comma() {
-        rowmatch("1,2,3,\"a,b,c\"",
-                 [["1", "2", "3", "a,b,c"]])
+        rowmatch(~"1,2,3,\"a,b,c\"",
+                 ~[~["1", "2", "3", "a,b,c"]])
     }
 
     #[test]
     fn blank_line() {
-        rowmatch("\n\n", [[], []]);
+        rowmatch(~"\n\n", ~[~[], ~[]]);
     }
 
     #[test]
     fn iter_test() {
-        let f = io::mem::MemReader::new("a brown,cat".into_bytes());
-        let r : rowreader = new_reader(f, ',', '"');
+        let s = ~"a brown,cat";
+        let f = ~MemReader::new(s.into_bytes());
+        let r : ~rowreader = new_reader(f, ',', '"');
         for row in r.iter() {
             assert!(row[0] == "a brown");
             assert!(row[1] == "cat");
